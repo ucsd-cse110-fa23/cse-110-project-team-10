0c9d2e0d820cdb4d6a3fbca53fe248c27ef5199f
chaup15 <c6pham@ucsd.edu>
2023-11-08 23:01:15 -0800

    issue #43 fixed ui

--- app/src/test/java/cse110_project/VoiceTest.java
+++ app/src/test/java/cse110_project/VoiceTest.java
@@ -3,6 +3,8 @@ package cse110_project;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import org.junit.jupiter.api.BeforeEach;
 
 public class VoiceTest {
@@ -21,6 +23,16 @@ public class VoiceTest {
         String expected = "user response";
         assertEquals(expected, api.handleVoiceInput(null));
     }
-    
+
+    /**
+     * Mock Whisper test taking in audio file and display response
+     */
+    @Test
+    public void testDisplayWithRecording() throws Exception{
+        String expected = "user response";
+        File audio = new File("recording.wav");
+        String result = api.handleVoiceInput(audio);
+        assertEquals(expected, result);
+    }
 }
 
