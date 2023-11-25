81ce323052cf7d9f43c3d0dbb44e95d7315a5bb1
StrawberryAkai <zihun1392@gmail.com>
2023-11-24 23:57:05 -0800

    Issue 57

--- app/src/main/java/cse110_project/CreateAcc.java
+++ app/src/main/java/cse110_project/CreateAcc.java
@@ -39,6 +39,7 @@ class CreateBox extends VBox {
     private Info username;
     private Info password;
     private Info confirmPass;
+
     private boolean isMatch;
 
     CreateBox(){
@@ -49,6 +50,7 @@ class CreateBox extends VBox {
         confirmPass = new Info("Confirm Password: ");
 
         this.getChildren().addAll(username, password, confirmPass);
+        this.setAlignment(Pos.CENTER);
     }
 
     public void checkPass(){
@@ -59,17 +61,102 @@ class CreateBox extends VBox {
             isMatch = false;
         }
     }
+
+    public Info userInfo(){
+        return username;
+    }
+
+    public Info passInfo(){
+        return password;
+    }
+}
+
+class Footer extends HBox {
+
+    private Button checkValidAccButton;
+    private Button createAccButton;
+
+    Footer(){
+        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
+
+        this.setPrefSize(500, 60);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        this.setSpacing(10);
+
+        checkValidAccButton = new Button("Check Valid");
+        checkValidAccButton.setStyle(defaultButtonStyle);
+
+        createAccButton = new Button("Create Account");
+        createAccButton.setStyle(defaultButtonStyle);
+
+        this.getChildren().addAll(checkValidAccButton, createAccButton);
+        this.setAlignment(Pos.CENTER);
+    }
+
+    public Button getCheckValidAccButton() {
+        return checkValidAccButton;
+    }
+
+    public Button getCreateAccButton() {
+        return createAccButton;
+    }
+}
+
+class Header extends HBox {
+
+    Header() {
+        this.setPrefSize(500, 60); // Size of the header
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        this.setPadding(new Insets(100,0,0,0));
+        Text titleText = new Text("PantryPal 2"); // Text of the Header
+        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 45;");
+        this.getChildren().add(titleText);
+        this.setAlignment(Pos.CENTER); // Align the text to the Center
+    }
 }
 
 class CreateScreen extends BorderPane {
     private CreateBox cBox;
-    private Button createButton;
+    private Footer footer;
+    private Header header;
+
+    private Button checkValidAccButton;
+    private Button createAccButton;
 
     CreateScreen(){
         cBox = new CreateBox();
+        footer = new Footer();
+        header = new Header();
 
+        this.setTop(header);
         this.setCenter(cBox);
+        this.setBottom(footer);
 
+        checkValidAccButton = footer.getCheckValidAccButton();
+        createAccButton = footer.getCreateAccButton();
+
+        addListeners();
+    }
+
+    public void addListeners(){
+
+        /*
+         *  set to taggled
+         *  write the infomation in a json file
+         *  
+         */
+        createAccButton.setOnAction(e -> {
+            //TODO
+        });
+
+        /*
+         * Check two things:
+         *  Confirm pass is the same as pass
+         *  Username is not duplicate
+         */
+        checkValidAccButton.setOnAction(e -> {
+            //TODO
+        });
     }
 }
 
@@ -83,7 +170,7 @@ public class CreateAcc extends Application {
 
         CreateScreen root = new CreateScreen();
 
-        Scene scene = new Scene(root, 1280, 720);
+        Scene scene = new Scene(root, 800, 600);
 
         primaryStage.setTitle("Create Account");
         primaryStage.setScene(scene);
